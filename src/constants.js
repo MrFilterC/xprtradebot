3d754b5e77004bfc708a0f69447dace6c5e0385d
MrfilterC <mrfilterc@gmail.com>
2025-05-17 14:28:12 +0300

    Log VITE_PROXY_URL for debugging

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-export const PROXY_URL = "http://localhost:3000";
+console.log("VITE_PROXY_URL from import.meta.env:", import.meta.env.VITE_PROXY_URL);
+
+export const PROXY_URL = import.meta.env.VITE_PROXY_URL || "http://localhost:3000";
 export const RPC_ENDPOINT = "https://mainnet.helius-rpc.com/?api-key=23750c00-b047-4444-84ae-39dae001dccd"; 
